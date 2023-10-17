c4785502b4a2cf3228eff8892f7a22f599d2ff61
Jefferson <jefferson.brandao@g.globo>
2023-10-17 10:18:46 -0300

    1 dia da imersão

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import React from "react";
+import { Card } from "./card";
+import { Button } from "./button";
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Separator } from "./separator";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+
+export const Header = () => {
+  const { status, data } = useSession();
+
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+
+  const handleLogoutClick = async () => {
+    await signOut();
+  };
+
+  return (
+    <header>
+      <Card className="flex items-center justify-between p-[1.875rem]">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent side="left">
+            <SheetHeader className="text-left text-lg font-semibold">
+              Menu
+            </SheetHeader>
+
+            {status === "authenticated" && data?.user && (
+              <div className="flex flex-col">
+                <div className="flex items-center gap-2 py-4">
+                  <Avatar>
+                    <AvatarFallback>
+                      {data.user.name?.[0].toUpperCase()}
+                    </AvatarFallback>
+
+                    {data.user.image && <AvatarImage src={data.user.image} />}
+                  </Avatar>
+
+                  <div className="flex flex-col">
+                    <p className="font-medium">{data.user.name}</p>
+                    <p className="text-sm opacity-75">Boas compras!</p>
+                  </div>
+                </div>
+
+                <Separator />
+              </div>
+            )}
+
+            <div className="mt-4 flex flex-col gap-2">
+              {status === "unauthenticated" && (
+                <Button
+                  onClick={handleLoginClick}
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <LogInIcon size={16} />
+                  Fazer Login
+                </Button>
+              )}
+
+              {status === "authenticated" && (
+                <Button
+                  onClick={handleLogoutClick}
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <LogOutIcon size={16} />
+                  Fazer Logout
+                </Button>
+              )}
+
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <HomeIcon size={16} /> Inicio
+              </Button>
+
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <PercentIcon size={16} /> Ofertas
+              </Button>
+
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <ListOrderedIcon size={16} /> Catálogo
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
+
+        <h1 className="text-lg font-semibold">
+          <span className="text-primary">Jeje</span> Store
+        </h1>
+
+        <Button size="icon" variant="outline">
+          <ShoppingCartIcon />
+        </Button>
+      </Card>
+    </header>
+  );
+};
